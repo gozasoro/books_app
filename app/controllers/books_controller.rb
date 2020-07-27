4662aa1f95a1f3d32938835c4320912e76576363
gozasoro <62997922+gozasoro@users.noreply.github.com>
2020-07-27 23:04:02 +0900

    i18nで英語と日本語に対応

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 class BooksController < ApplicationController
+  before_action :set_locale
   before_action :set_book, only: %i[show edit update destroy]
 
   # GET /books
-  # GET /books.json
   def index
     @books = Book.all
   end
 
   # GET /books/1
-  # GET /books/1.json
   def show; end
 
   # GET /books/new
@@ -22,46 +21,44 @@ class BooksController < ApplicationController
   def edit; end
 
   # POST /books
-  # POST /books.json
   def create
     @book = Book.new(book_params)
 
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to @book, notice: "Book was successfully created." }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.save
+      redirect_to @book, notice: t("flash.created")
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
   def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: "Book was successfully updated." }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.update(book_params)
+      redirect_to @book, notice: t("flash.updated")
+    else
+      render :edit
     end
   end
 
   # DELETE /books/1
-  # DELETE /books/1.json
   def destroy
     @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to books_url, notice: t("flash.destroyed")
   end
 
   private
+    def set_locale
+      I18n.locale = locale_in_params || I18n.default_locale
+    end
+
+    def locale_in_params
+      if params[:locale].present?
+        params[:locale].to_sym.presence_in(I18n.available_locales)
+      else
+        nil
+      end
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_book
       @book = Book.find(params[:id])
@@ -70,5 +67,9 @@ class BooksController < ApplicationController
     # Only allow a list of trusted parameters through.
     def book_params
       params.require(:book).permit(:title, :memo, :author, :picture)
+    end
+
+    def default_url_options
+      { locale: I18n.locale }
     end
 end
