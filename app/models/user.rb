f3dd3282ff80e2520ec434fae9e4bbb96ea812b2
gozasoro <62997922+gozasoro@users.noreply.github.com>
2020-08-23 00:46:13 +0900

    omniauthでGitHub認証を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
   validates :postal_code, format: { with: /\A(\d{3}[-]\d{4})?\z/, message: "半角数字7桁（ハイフンあり）で入力してください" }
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.name = auth.info.nickname
+      user.email = User.dummy_email(auth)
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
+
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_141852) do
+ActiveRecord::Schema.define(version: 2020_08_22_124002) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -34,7 +34,10 @@ ActiveRecord::Schema.define(version: 2020_08_14_141852) do
     t.string "address"
     t.text "introduction"
     t.string "name"
+    t.string "uid", null: false
+    t.string "provider", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 end
