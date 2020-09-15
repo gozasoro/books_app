c1ad1532c27145aa0733ad59abc501f193bb732a
gozasoro <62997922+gozasoro@users.noreply.github.com>
2020-09-16 02:33:00 +0900

    フォロー機能を追加

---
+++ db/migrate/20200917114127_change_columns_add_notnull_on_relationships.rb
@@ -0,0 +1,6 @@
+class ChangeColumnsAddNotnullOnRelationships < ActiveRecord::Migration[6.0]
+  def change
+    change_column :relationships, :following_id, :integer, null: false
+    change_column :relationships, :follower_id, :integer, null: false
+  end
+end
